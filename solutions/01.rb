01de0887baf4c3d8a606ab4c1b221157384cfe7c
malinad <malina.demirova@gmail.com>
2016-01-16 17:14:57 +0200

    Change in task 1 - using fetch to catch the value

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,7 +1,7 @@
 def convert_to_bgn(price, currency)
        currencies  = { usd: 1.7408, eur: 1.9557, gbp: 2.6415, bgn: 1}
-	   
-		(price * currencies[currency]).round(2)
+	   value = currencies.fetch(currency)
+		(price * value).round(2)
 end
 
 def compare_prices(price_one, currency_one, price_other, currency_other)
