da6c009cf73f55d68deef5c6162742b483971d30
malinad <malina.demirova@gmail.com>
2016-01-16 16:31:42 +0200

    Changes in task 1 replacing the case with hash operation

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,14 +1,10 @@
 def convert_to_bgn(price, currency)
-       case currency
-	        when :usd then rate = 1.7408
-			when :eur then rate = 1.9557
-			when :gbp then rate = 2.6415
-			else rate = 1
-		end
-        (price * rate).round(2)
+       currencies  = { :usd = 1.7408, :usd = 1.9557, :gbp = 2.6415, :bgn = 1}
+	   
+		(price * currencies[currency]).round(2)
 end
 
-def compare_prices(price_1, currency_1, price_2, currency_2)
-        convert_to_bgn(price_1, currency_1) <=> 
-		convert_to_bgn(price_2, currency_2)
+def compare_prices(price_one, currency_one, price_other, currency_other)
+        convert_to_bgn(price_one, currency_one) <=>
+		convert_to_bgn(price_other, currency_other)
 end
